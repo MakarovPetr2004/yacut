32f6a6fdab888c734b7d09facc97b16b7fa16df0
Макаров Пётр <mr.ru.20042004@gmail.com>
2024-02-03 00:14:06 +0300

    v 1.1.0 fixed env

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    FLASK_DEBUG = os.getenv('FLASK_DEBUG')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'test')
+    FLASK_DEBUG = os.getenv('FLASK_DEBUG', 'production')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
